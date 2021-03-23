963f85d06d2e72e4d403495e5b4b70c00da37c83
Bojan Ristic <e.tachka@gmail.com>
2021-03-23 12:55:43 +0100

    project skeleton

---
+++ src/Validator.elm
@@ -0,0 +1,148 @@
+module Validator exposing
+    ( Validator, run
+    , map, map2
+    , andMap, andThen
+    , custom, succeed, fail
+    , required, optional
+    )
+
+{-|
+
+
+# Definiion
+
+@docs Validator, run
+
+
+# Transform
+
+@docs map, map2
+
+@docs andMap, andThen
+
+@docs custom, succeed, fail
+
+@docs required, optional
+
+-}
+
+
+{-| Validator
+-}
+type Validator a e b
+    = Validator (a -> Result (List e) b)
+
+
+{-| Run validator
+-}
+run : Validator a e b -> a -> Result (List e) b
+run (Validator f) =
+    f
+
+
+{-| Create validator that always succeed
+-}
+succeed : b -> Validator a e b
+succeed =
+    Validator << always << Ok
+
+
+{-| Create validator that always fails
+-}
+fail : e -> Validator a e b
+fail =
+    Validator << always << Err << List.singleton
+
+
+{-| Build custom validator
+-}
+custom : (a -> Result (List e) b) -> Validator a e b
+custom =
+    Validator
+
+
+{-| Transform validated value
+-}
+map : (b -> c) -> Validator a e b -> Validator a e c
+map f (Validator g) =
+    Validator (Result.map f << g)
+
+
+{-| Transform validated value
+-}
+map2 : (b -> c -> d) -> Validator a e b -> Validator a e c -> Validator a e d
+map2 f (Validator g) (Validator h) =
+    Validator (\x -> Result.map2 f (g x) (h x))
+
+
+{-| Apply wrapped function on to another wrapped value
+-}
+andMap : Validator a e b -> Validator a e (b -> c) -> Validator a e c
+andMap =
+    map2 (|>)
+
+
+{-| Chain validators
+-}
+andThen : (b -> Validator a e c) -> Validator a e b -> Validator a e c
+andThen f g =
+    Validator
+        (\x ->
+            x
+                |> run g
+                |> Result.map f
+                |> Result.andThen (\h -> run h x)
+        )
+
+
+{-| Combine validators with required field
+-}
+required : (a -> b) -> (b -> Bool) -> e -> Validator b e c -> Validator a e (c -> d) -> Validator a e d
+required accessor isEmpty err (Validator f) (Validator h) =
+    Validator
+        (\x ->
+            x
+                |> (predicateMaybe isEmpty << accessor)
+                |> Result.fromMaybe [ err ]
+                |> Result.andThen f
+                |> applyResults (h x)
+        )
+
+
+{-| Combine validators with optional field
+-}
+optional : (a -> b) -> (b -> Bool) -> c -> Validator b e c -> Validator a e (c -> d) -> Validator a e d
+optional accessor isEmpty default (Validator f) (Validator h) =
+    Validator
+        (\x ->
+            x
+                |> (predicateMaybe isEmpty << accessor)
+                |> Maybe.map f
+                |> Maybe.map (applyResults (h x))
+                |> Maybe.withDefault (applyResults (h x) (Ok default))
+        )
+
+
+applyResults : Result (List e) (a -> b) -> Result (List e) a -> Result (List e) b
+applyResults r1 r2 =
+    case ( r1, r2 ) of
+        ( Ok f, Ok x ) ->
+            Ok (f x)
+
+        ( Err e, Err e_ ) ->
+            Err (e ++ e_)
+
+        ( Err e, _ ) ->
+            Err e
+
+        ( _, Err e ) ->
+            Err e
+
+
+predicateMaybe : (a -> Bool) -> a -> Maybe a
+predicateMaybe isEmpty x =
+    if isEmpty x then
+        Nothing
+
+    else
+        Just x
---
+++ tests/ValidatorTest.elm
@@ -0,0 +1,63 @@
+module ValidatorTest exposing (..)
+
+import Expect
+import Test exposing (Test, describe, test)
+import Validator exposing (Validator)
+
+
+suite : Test
+suite =
+    describe "Validation test"
+        [ test "validate happy hello!" <|
+            \_ ->
+                let
+                    happy =
+                        Validator.succeed identity
+                            |> Validator.required identity String.isEmpty "No hello!" (Validator.custom Ok)
+                in
+                Expect.equal (Validator.run happy "hello!") (Ok "hello!")
+        , test "Unhappy message" <|
+            \_ ->
+                let
+                    unhappy =
+                        Validator.succeed (\x -> { hello = x })
+                            |> Validator.required .hello String.isEmpty "No hello!" (Validator.custom Ok)
+                in
+                Expect.equal (Validator.run unhappy { hello = "" }) (Err [ "No hello!" ])
+        , test "Unhappy record" <|
+            \_ ->
+                let
+                    unhappy =
+                        Validator.succeed (\x y -> { a = x, b = y })
+                            |> Validator.required .a String.isEmpty "a is required" (Validator.custom Ok)
+                            |> Validator.required .b String.isEmpty "b is required" (Validator.custom Ok)
+                in
+                Expect.equal (Validator.run unhappy { a = "", b = "" }) (Err [ "a is required", "b is required" ])
+        , test "Mixed record" <|
+            \_ ->
+                let
+                    mixed =
+                        Validator.succeed (\x y -> { a = x, b = y })
+                            |> Validator.required .a String.isEmpty "a is required" (Validator.custom Ok)
+                            |> Validator.required .b String.isEmpty "b is required" (Validator.custom Ok)
+                in
+                Expect.equal (Validator.run mixed { a = "hello!", b = "" }) (Err [ "b is required" ])
+        , test "Optional happy saylor" <|
+            \_ ->
+                let
+                    optionaHappy =
+                        Validator.succeed (\x y -> { a = x, b = y })
+                            |> Validator.required .a String.isEmpty "a is required" (Validator.custom Ok)
+                            |> Validator.optional .b String.isEmpty "saylor" (Validator.custom Ok)
+                in
+                Expect.equal (Validator.run optionaHappy { a = "hello", b = "" }) (Ok { a = "hello", b = "saylor" })
+        , test "Optional unhappy saylor" <|
+            \_ ->
+                let
+                    optionaUnhappy =
+                        Validator.succeed (\x y -> { a = x, b = y })
+                            |> Validator.required .a String.isEmpty "a is required" (Validator.custom Ok)
+                            |> Validator.optional .b String.isEmpty 0 (Validator.custom (String.toInt >> Result.fromMaybe [ "b is not a number" ]))
+                in
+                Expect.equal (Validator.run optionaUnhappy { a = "hello", b = "saylor" }) (Err [ "b is not a number" ])
+        ]
